29b34f59bec9610cba95142a94c69ee21be2da95
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2026-02-04 17:11:53 +0100

    Add separate function prepare_for_clustering() for preprocessing. Also adjust the explanation-objective to define the best number of classes.

--- R/cluster_analysis.R
+++ R/cluster_analysis.R
@@ -2,62 +2,83 @@
 #' 
 #' Based on the reduction of within cluster sum of squares
 #' 
-#' @param ts_table A table of timeseries data per pixel created by 
-#' [dynamic_per_pixel()]
+#' @param dpp The output of [dynamic_per_pixel()]
 #' @param kMax Tha maximum number of clusters to be tested
-#' @param improvement_objective The improvement of within cluster sum of squares
-#' (WCSS) by addition of one cluster in %.
+#' @param explanation_objective The a threshold between 0 and 1 for the ratio 
+#' between Within-Cluster sum of squares and Between-Cluster sum of squares.
 #' @param plot_result Logical
+#' @param correlate_first If TRUE, Pearson correlation between pixels will be 
+#' used for clustering instead of Index values. This results in a clustering 
+#' based on different shapes of the dynamic. 
+#' 
+#' @details
+#' The search for the best number of clusters is done by a maximum of 10 000 
+#' pixels, which is assumed to be sufficient. If correlation is done first
+#' those 10 000 pixels are correlated with 1000 pixels of the same data. 
+#' Furthermore, the number of observerations (365 days) is reduced to every 
+#' third day only, to lower process time. This way, correlation should take
+#' less than 10 seconds.
+#' 
 #' 
 #' @return This function plots the improvement values and returns the recommended
 #' number of cluster to use
 #' 
-#' @importFrom stats kmeans
+#' @importFrom stats kmeans cor
 #' 
 #' @export
 #' 
 best_nk <- function(
-    ts_table, kMax = 10, improvement_objective = 10, plot_result = TRUE
+    dpp, kMax = 10, explanation_objective = 0.98, plot_result = TRUE, correlate_first = FALSE
 ){
-  ts_table <- ts_table[,-1]
-  if(ncol(ts_table) > 10000){
-    set.seed(1)
-    ts_table <- ts_table[,sample(1:ncol(ts_table), size = 10000)]
-  }
-  
-  na_values <- sapply(ts_table, function(x){any(is.na(x))})
-  remove_pixel <- which(na_values)
-  if(length(remove_pixel) > 0L){
-    ts_table <- ts_table[,-remove_pixel]
-    warning(length(remove_pixel), " Pixels contained NA values and were removed",
-            " prior to cluster analysis")
-  }
+  y <- prepare_for_clustering(
+    moving_averages_matrix = dpp$moving_averages[,-1], 
+    correlate_first = correlate_first, 
+    maxPixels = 10000
+  )
   
   wcss <- c()
-  for(i in 1:kMax){
+  withinBetween <- 0
+  i <- 1
+  while(withinBetween < explanation_objective){
+    if(i > kMax){
+      break
+    }
     cat(paste0("Calculation of ", i, ifelse(i == 1, " cluster", " clusters"), " ... \n"))
-    kmeansOutput <- kmeans(t(ts_table), centers = i, iter.max = 20, nstart = 10)
-    wcss <- c(wcss, kmeansOutput$tot.withinss)
+    
+    kmeansOutput <- kmeans(t(y), centers = i, iter.max = 20, nstart = 2)
+    
+    withinBetween <- kmeansOutput$betweenss / kmeansOutput$totss
+    wcss <- c(wcss, withinBetween)
+    i <- i + 1
   }
-  wcss_improve <- -(diff(wcss) / wcss[1:(length(wcss) - 1)] * 100)
-  plot(
-    x = 2:length(wcss), 
-    y = wcss_improve, 
-    ylim = c(0,max(wcss_improve)),
-    type = "l", 
-    ylab = "Within Cluster Sum of Squares Improvement in %", 
-    xlab = "Number of Clusters", 
-    lwd = 2, main = "Identification of best number of clusters")
   
-  best_nCluster <- max(seq_along(wcss_improve)[wcss_improve > improvement_objective]) + 1
+  par(mar = c(4.1, 4.1, 4.1, 2.1))
+  plot(
+    x = 1:length(wcss), 
+    y = wcss*100, 
+    ylim = c(0,100),
+    type = "l", 
+    ylab = "%", 
+    xlab = "Number of Clusters", 
+    lwd = 2, main = "Variance Explained by Clusters")
+  
+  best_nCluster <- length(wcss)
+  
   abline(v = best_nCluster)
-  abline(h = improvement_objective)
-  text(x = best_nCluster, y = 0, 
-       labels = paste0("Recommended number of clusters = ", best_nCluster), pos = 4)
+  abline(h = explanation_objective * 100, lty = "dotted")
+  
+  tp <- ifelse(
+    abs(best_nCluster - par("xaxp")[2]) < abs(best_nCluster - par("xaxp")[1]), 
+    yes = 2, 
+    no = 4
+  )
+  text(x = best_nCluster, 
+       y = 0, 
+       labels = paste0("Recommended number of clusters = ", best_nCluster), 
+       pos = tp
+  )
   best_nCluster
 }
-
-
 
 #' cluster analysis (k-means) and map layer
 #' 
@@ -65,27 +86,47 @@ best_nk <- function(
 #' @param nc The netCDF data list created by [load_netcdf()]
 #' @param k The number of Clusters
 #' @param iter.max,nstart Arguments of [kmeans()]
+#' @param correlate_first If TRUE, Pearson correlation between pixels will be 
+#' used for clustering instead of Index values. This results in a clustering 
+#' based on different shapes of the dynamic. 
+#' @param whole_dynamic If TREU all 365 days average values will be used to 
+#' compare pixels. Otherwise the number of days will be reduced if the number
+#' of pixels is too high (> 100 000).
 #' 
 #' @return A list of the k-means output and a layer of clusted pixels
 #' 
-#' @importFrom stats kmeans
+#' @importFrom stats kmeans cor
 #' 
 #' @export
 #' 
 pixel_clusters <- function(
-    dpp, nc, k, iter.max = 20, nstart = 10
+    dpp, nc, k, iter.max = 20, nstart = 10, correlate_first = FALSE, 
+    whole_dynamic = FALSE
 ){
-  na_values <- sapply(dpp$moving_averages, function(x){any(is.na(x))})
-  remove_pixel <- which(na_values)
-  if(length(remove_pixel) > 0L){
-    dpp$moving_averages <- dpp$moving_averages[,-remove_pixel]
-    warning(length(remove_pixel), " Pixels contained NA values and were removed",
-            " prior to cluster analysis")
+  
+  keep <- 1:365
+  if(!whole_dynamic){
+    proportionToRemove <- 100000 / ncol(dpp$moving_averages)
+    i_d <- round(1 / proportionToRemove)
+    if(i_d > 10){
+      i_d <- 10
+    }
+    keep <- seq(i_d, 365, i_d)
+    dpp$moving_averages <- dpp$moving_averages[keep,]
+    cat(paste0("due to large pixel number ", 365 - length(keep), 
+               " days removed before clustering ... \n")
+    )
   }
-  # rounding data points to smooth out differences of very similar data points
-  x <- signif(dpp$moving_averages[,-1], 3)
+  
+  y <- prepare_for_clustering(
+    moving_averages_matrix = dpp$moving_averages[,-1], 
+    correlate_first = correlate_first, 
+    maxPixels = NULL
+  )
+  
+  cat(paste0("clustering into ", k, " cluster ... \n"))
   kmeansOutput <- kmeans(
-    t(x), centers = k, iter.max = iter.max, nstart = nstart
+    x = t(y), centers = k, iter.max = iter.max, nstart = nstart
   )
   
   m_init <- matrix(data = 0, nrow = length(nc$y), ncol = length(nc$x))
@@ -98,7 +139,62 @@ pixel_clusters <- function(
   
   list(
     "kmeans" = kmeansOutput,
-    "cluster_layer" = m_init)
+    "cluster_layer" = m_init,
+    "days_included" = keep)
 }
+
+#' Pre-processing done before cluster analysis
+#' 
+#' @param moving_averages_matrix A matrix of p pixels as column and 365 days as
+#' rows of the moving averages of one index as created by [dynamic_per_pixel()]
+#' @param correlate_first If TRUE, Pearson correlation between pixels will be 
+#' used for clustering instead of Index values. This results in a clustering 
+#' based on different shapes of the dynamic. 
+#' @param maxPixels The maximum numbers of pixel used for cluster analysis.
+#' Leave NULL to use all pixels.
+#' 
+#' @return A matrix either with data per pixels or correlation between pixels
+#' used for following cluster analysis
+#' 
+#' @importFrom stats cor
+#' 
+#' @export
+#' 
+prepare_for_clustering <- function(
+    moving_averages_matrix, correlate_first = FALSE, maxPixels = NULL
+){
+  ts_table <- signif(moving_averages_matrix[,-1], 3)
+  
+  na_values <- colSums(is.na(ts_table))
+  ts_table <- ts_table[,(na_values == 0L)]
+  nna <- sum(na_values > 0L)
+  if(nna > 0L){
+    warning(nna, " Pixels contained NA values and were removed",
+            " prior to cluster analysis")
+  }
+  
+  if(!is.null(maxPixels)){
+    set.seed(1)
+    ts_table <- ts_table[,sample(
+      x = 1:ncol(ts_table), 
+      size = min(maxPixels, ncol(ts_table))
+    )]
+  }
+  
+  if(correlate_first){
+    ts_reference <- ts_table
+    if(ncol(ts_reference) > 1000){
+      set.seed(2)
+      ts_reference <- ts_reference[,sample(1:ncol(ts_reference), size = 1000)]
+    }
+    cat(paste0("correlation of ", ncol(ts_table), " pixels ... \n"))
+    y <- t(cor(x = ts_table, y = ts_reference, use = "pairwise.complete.obs"))
+    y <- round(y, 5)
+  } else {
+    y <- ts_table
+  }
+  y
+}
+
 
 
